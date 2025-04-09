134570f8b9453aacb125ff169724219fdaaeed34
Gianni <132337749+giannindm@users.noreply.github.com>
2025-04-09 20:53:54 +0200

    Add function to generate computer choice

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+// initialize both scores
+
+// generate computer choice
+function getComputerChoice() {
+    const num = Math.floor(Math.random() * 3)
+    switch (num) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break
+        case 2:
+            return "scissors";
+            break
+    }
+}
+
+// prompt user choice
+
+// play round
+// compare user choice against computer choice
+// if there is a winner, increment winner score and declare winner of round
+// if there is a tie, then generate a new computer choice, and prompt a new user choice, then compare choices again
