b97d9dbe328ded1acb680b06a50b05e8965d221f
Gianni <132337749+giannindm@users.noreply.github.com>
2025-04-17 15:23:52 +0200

    Add UI

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// initialize scores
+playerScore = 0;
+computerScore = 0;
+const spanPlayerScore = document.createElement("span");
+const spanComputerScore = document.createElement("span");
+const paraChoices = document.createElement("p");
+
+// get relevant DOM elements
+const card = document.querySelector(".card");
+const options = document.querySelector(".options");
+const divRock = document.querySelector(".rock");
+const divPaper = document.querySelector(".paper");
+const divScissors = document.querySelector(".scissors");
+const btnRock = document.querySelector(".btnRock");
+const btnPaper = document.querySelector(".btnPaper");
+const btnScissors = document.querySelector(".btnScissors");
+const paraPlayerScore = document.querySelector(".playerScore");
+const paraComputerScore = document.querySelector(".computerScore");
+const divChoices = document.querySelector(".choices");
+
+// listen for button clicks
+btnRock.addEventListener("click", function (e) {
+    playRound(e.target.className);
+});
+btnPaper.addEventListener("click", function (e) {
+    playRound(e.target.className);
+});
+btnScissors.addEventListener("click", function (e) {
+    playRound(e.target.className);
+});
+
 // generate computer choice
 function getComputerChoice() {
     const num = Math.floor(Math.random() * 3)
@@ -14,51 +45,80 @@ function getComputerChoice() {
     }
 }
 
-// prompt player choice
-function getPlayerChoice() {
-    return prompt("Choose rock, paper or scissors").toLowerCase();
+// get player choice based on button clicked
+function getPlayerChoice(btnClicked) {
+    if (btnClicked === "btnRock") {
+        return "rock";
+    } else if (btnClicked === "btnPaper") {
+        return "paper";
+    } else if (btnClicked === "btnScissors") {
+        return "scissors";
+    }
 }
 
 
 // play round
-function playRound() {
-    let winner = "";
-    const playerChoice = getPlayerChoice();
+function playRound(btnClicked) {
+    const playerChoice = getPlayerChoice(btnClicked);
     const computerChoice = getComputerChoice();
-    console.log(`You played ${playerChoice} and the computer played ${computerChoice}`);
+    
+    paraChoices.textContent = `You played ${playerChoice} and the computer played ${computerChoice}.`;
 
 // compare player choice against computer choice
 // increment score of winner and log who won
     if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
-        winner = "player";
         playerScore += 1;
-        console.log(`The ${winner} wins this round.`);
+        spanPlayerScore.textContent = playerScore;
+        paraPlayerScore.appendChild(spanPlayerScore);
+        paraChoices.textContent += ' You win.'
     }  else if ((computerChoice === "rock" && playerChoice === "scissors") || (computerChoice === "paper" && playerChoice === "rock") || (computerChoice === "scissors" && playerChoice === "paper")) {
-        winner = "computer";
         computerScore += 1;
-        console.log(`The ${winner} wins this round.`);
+        spanComputerScore.textContent = computerScore;
+        paraComputerScore.appendChild(spanComputerScore);
+        paraChoices.textContent += ' The computer wins.'
     } else {
-        console.log("It's a tie! Try again");
-        playRound();
+        paraChoices.textContent += ' It\'s a tie. Play again.'
+    }
+
+    divChoices.appendChild(paraChoices);
+
+    if (playerScore === 5) {
+        const winMsg = document.createElement("p");
+        winMsg.textContent = ("You win!");
+        winMsg.setAttribute("class", "winMsg");
+        card.appendChild(winMsg);
+        endGame();
+    } else if (computerScore === 5) {
+        const winMsg = document.createElement("p");
+        winMsg.textContent = ("The computer wins :(");
+        winMsg.setAttribute("class", "winMsg");
+        card.appendChild(winMsg);
+        endGame();
     }
 }
 
-// Play a game of 5 rounds, announcing each round before it starts
-function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        console.log(`We are now playing round ${i}!`)
-        playRound();
-        console.log(`Currently the score is:
-            player: ${playerScore}
-            computer: ${computerScore}`);
-    }
-    console.log(`The final score is:
-        player: ${playerScore}
-        computer: ${computerScore}`);
-    playerScore > computerScore ? console.log("You win!") : console.log("The computer wins.");
+function endGame() {
+    btnRock.remove();
+    btnPaper.remove();
+    btnScissors.remove();
+    const btnPlayAgain = document.createElement("button");
+    btnPlayAgain.textContent = "Play again.";
+    options.appendChild(btnPlayAgain);
+    btnPlayAgain.addEventListener("click", () => {
+        playerScore = 0;
+        computerScore = 0;
+        spanPlayerScore.textContent = playerScore;
+        spanComputerScore.textContent = computerScore;
+        const winMsg = document.querySelector(".winMsg");
+        winMsg.remove();
+        btnPlayAgain.remove();
+        options.appendChild(btnRock);
+        options.appendChild(btnPaper);
+        options.appendChild(btnScissors);
+    });
 }
 
-// Initialize scores and play game
-let playerScore = 0;
-let computerScore = 0;
-playGame();
+function newGame() {
+    playerScore = 0;
+    computerScore = 0;
+}
